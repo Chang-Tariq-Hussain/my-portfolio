e80f22bb6aa846f90daf3dc5085d1403a3ecef1f
Chang-Tariq-Hussain <chang.tariq48@gmail.com>
2025-03-15 10:55:00 -0700

    Add files via upload
    setup tailwindcss and shadcn in next js

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import {Button} from '@/components/ui/button'
+export default function Home() {
+  return (
+      <div className="min-h-screen flex items-center justify-center">
+          <Button variant={"destructive"} className={"dark cursor-pointer"}>Click me!</Button>
+      </div>
+  );
+}
