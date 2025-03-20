fb5e6517b9e6475127561a1cde32c0b2bb8403b3
Tariq <chang.tariq48@gmail.com>
2025-03-20 02:29:36 -0700

    header component completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
-import {getSkills} from "@/sanity/lib/skill/Skills";
-import {Skill} from "../../sanity.types";
-import Image from "next/image";
-import {urlFor} from "@/sanity/lib/image";
+// import {getSkills} from "@/sanity/lib/skill/Skills";
+// import {Skill} from "../../sanity.types";
+// import Image from "next/image";
+// import {urlFor} from "@/sanity/lib/image";
 export default async function Home() {
-    const skills:Skill[] = await getSkills();
+    // const skills:Skill[] = await getSkills();
   return (
-      <div className="min-h-screen flex items-center justify-center">
-          <ul>
-              {skills.map((skill) => <Image key={skill._id} alt={skill.name || "skill"} width={300} height={300} src={urlFor(skill.icon).url() || ""}></Image>)}
-          </ul>
+      <div className="min-h-screen bg-white dark:bg-gray-900">
+          {/*<ul>*/}
+          {/*    {skills.map((skill) => <Image key={skill._id} bg-gray-800alt={skill.name || "skill"} width={300} height={300} src={urlFor(skill.icon).url() || ""}></Image>)}*/}
+          {/*</ul>*/}
       </div>
   );
 }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,8 +2,13 @@ import ToggleThemeMode from "@/components/ToggleThemeMode";
 
 export default function Header() {
     return (
-        <div>
-            <ToggleThemeMode/>
+        <div className={'fixed top-0 z-20 w-full flex items-center h-16  shadow-sm bg-white dark:bg-gray-950 inset-shadow-gray-950 transition-colors duration-300'}>
+            <div className={'flex items-center justify-between w-[98%] mx-auto md:w-[93%] lg:[w-90%] xl:w-[85%]'}>
+                <div className={'px-4 py-2 border-b-4 border-r-4 border-b-green-800 border-r-green-500'}>
+                    <strong>Dev.Tariq</strong>
+                </div>
+                <ToggleThemeMode/>
+            </div>
         </div>
     )
 }
